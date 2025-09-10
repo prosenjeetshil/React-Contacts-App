4c2156aa8d0b68e15c62577d18f7a0b1903da2ea
prosenjeetshil <prosenjeetshil@gmail.com>
2025-09-10 15:57:07 +0530

    feat: ensure contact removal is processed through API

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import * as ContactsAPI from "./utils/ContactsAPI"
 const App = () => {
 
     const removeContact = (contact) => {
+        ContactsAPI.remove(contact);
         setContacts(contacts.filter(c => c.id != contact.id))
     }
 
