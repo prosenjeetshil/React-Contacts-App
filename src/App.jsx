53560152aaa654f1fe866cf52bff46e60e366a7f
prosenjeetshil <prosenjeetshil@gmail.com>
2025-09-10 20:26:31 +0530

    feat: enhance CreateContacts component with form submission handling and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ListContacts from "./components/ListContacts";
-import * as ContactsAPI from "./utils/ContactsAPI"
+import * as ContactsAPI from "./utils/ContactsAPI";
 import CreateContacts from "./components/CreateContacts";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 const App = () => {
+  let navigate = useNavigate();
 
-    const removeContact = (contact) => {
-        ContactsAPI.remove(contact);
-        setContacts(contacts.filter(c => c.id != contact.id))
-    }
+  const removeContact = (contact) => {
+    ContactsAPI.remove(contact);
+    setContacts(contacts.filter((c) => c.id != contact.id));
+  };
 
-    const [contacts, setContacts] = useState([]);
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+      setContacts(contacts.concat(res));
+    };
+    create();
+    navigate("/");
+  };
 
-    useEffect(() => {
-        const getContacts = async () => {
-            const res = await ContactsAPI.getAll();
-            setContacts(res);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await ContactsAPI.getAll();
+      setContacts(res);
+    };
+
+    getContacts();
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <ListContacts contacts={contacts} onDeleteContact={removeContact} />
         }
-
-        getContacts();
-    }, []);
-
-    return (
-        <Routes>
-            <Route exact path="/" element={<ListContacts contacts={contacts} onDeleteContact={removeContact}/>}/>
-            <Route path="/create" element={<CreateContacts/>}/>
-        </Routes>
-    )
+      />
+      <Route
+        path="/create"
+        element={
+          <CreateContacts
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
+    </Routes>
+  );
 };
 
 export default App;
--- src/components/CreateContacts.jsx
+++ src/components/CreateContacts.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ImageInput from './ImageInput'
 import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
 
-const CreateContacts = () => {
+const CreateContacts = ({onCreateContact}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = serializeForm(e.target, {hash: true});
+
+        if(onCreateContact){
+            onCreateContact(value);
+            
+        }
+    }
   return (
     <div>
         <Link className='close-create-contact' to='/'>Close Contacts</Link>
-        <form className='create-contact-form'> 
+        <form className='create-contact-form' onSubmit={handleSubmit}> 
             <ImageInput className='create-contact-avatar-input' name='avatarURL' maxHeight={64} />
             <div className='create-contact-details'>
                 <input type='text' name='name' placeholder='Enter Name' />
